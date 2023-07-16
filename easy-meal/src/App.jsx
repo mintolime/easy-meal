c871c810323f77ef0dfb8476ab8a7a5d49a80d70
Hi-lo <104442896+mintolime@users.noreply.github.com>
2023-07-16 10:47:54 +0000

    dev: inst react router dom + added footer

--- easy-meal/src/App.jsx
+++ easy-meal/src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 
 function App() {
-  console.log('im update')
+  console.log("im update");
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
--- easy-meal/src/components/Main/Main.jsx
+++ easy-meal/src/components/Main/Main.jsx
@@ -14,7 +14,6 @@ function Main() {
       <div className="main__img-box">
         {/* <img className="main__photo-plate" src={plate} alt="блюдо с авокадо" />
         <img className="main__photo-rucola" src={rucola} alt="руккола" /> */}
-
         <img className="main__photo-plate" src={photo} alt="блюдо с авокадо" />
       </div>
     </main>
