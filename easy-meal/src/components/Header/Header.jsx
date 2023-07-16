f754209f0167b25196f16d8001a6f1ec1b1870b3
Hi-lo <104442896+mintolime@users.noreply.github.com>
2023-07-16 09:07:46 +0000

    adaptive layout Header , Main comp

--- easy-meal/src/components/Header/Header.jsx
+++ easy-meal/src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import './Header.css';
-
+import logo from '../../images/header-logo.svg'
+import Button from '../Button/Button';
 function Header() {
     return (
         <header className='header'>
           <img className="header__logo" src={logo} alt="логотип шапки сайта" />
+          <Button btnClass={'button_type_menu-open'} />
         </header>
     )
 }
---
+++ easy-meal/src/components/Main/Main.jsx
@@ -0,0 +1,24 @@
+import Button from "../Button/Button";
+import plate from "../../images/plate.png";
+import rucola from "../../images/rucola-png.png";
+import photo from "../../images/Photo.png";
+import "./Main.css";
+
+function Main() {
+  return (
+    <main className="main">
+      <div className="main__content">
+        <h1 className="main__title">Рецепты в один клик</h1>
+        <Button btnClass={"button_type_start"} btnText="Начать" />
+      </div>
+      <div className="main__img-box">
+        {/* <img className="main__photo-plate" src={plate} alt="блюдо с авокадо" />
+        <img className="main__photo-rucola" src={rucola} alt="руккола" /> */}
+
+        <img className="main__photo-plate" src={photo} alt="блюдо с авокадо" />
+      </div>
+    </main>
+  );
+}
+
+export default Main;
