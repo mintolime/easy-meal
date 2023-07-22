4c62c1d1020685626cb3bdefbf35cb52ab7780fc
Alexey Kudryavtsev <alveek@yandex.ru>
2023-07-22 16:56:01 +0300

    change port to 3003, update readme

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const { NODE_ENV, PORT, JWT_SECRET, CONNECT } = process.env;
 
 const config = {
   nodeEnv: NODE_ENV || 'development',
-  port: PORT || 3000,
+  port: PORT || 3003,
   jwtSecret: NODE_ENV === 'production' ? JWT_SECRET : 'not-so-secret',
   connectDbString: CONNECT || 'mongodb://127.0.0.1:27017/easymeal',
 };
