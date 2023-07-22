3950b36e117ad90ee8ba412756d5773b1925e881
Alexey Kudryavtsev <alveek@yandex.ru>
2023-07-22 16:56:56 +0300

    change port to 3003, update readme

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const connectDB = require('./db');
 // const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { NotFound } = require('./errors');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3003 } = process.env;
 const app = express();
 
 connectDB();
