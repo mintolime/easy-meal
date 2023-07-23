9d5fbfa8b87baf1dd6a0e8d22d66a37c769129a5
Hi-lo <104442896+mintolime@users.noreply.github.com>
2023-07-23 03:59:42 +0000

    style: fix login & register comp

--- easy-meal/src/components/Login/Login.jsx
+++ easy-meal/src/components/Login/Login.jsx
@@ -1,15 +1,15 @@
 // import React from 'react';
 
-import './Login.css';
-import { Form, Input } from 'antd';
-import Button from '../Button/Button';
+import "./Login.css";
+import { Form, Input } from "antd";
+import Button from "../Button/Button";
 
 const onFinish = (values) => {
-  console.log('Success:', values);
+  console.log("Success:", values);
 };
 
 const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
+  console.log("Failed:", errorInfo);
 };
 
 const Login = () => (
@@ -22,26 +22,39 @@ const Login = () => (
     // initialValues={{ remember: true }}
     onFinish={onFinish}
     onFinishFailed={onFinishFailed}
-    autoComplete="off">
-    <h3 className="form__title">Login</h3>
+    autoComplete="off"
+  >
+    <h3 className="form__title">Вход</h3>
 
     <Form.Item
-      label="Username"
-      name="username"
-      rules={[{ required: true, message: 'Please input your username!' }]}>
-      <Input className="form__imput" />
+      className="form__inner"
+      name="email"
+      label="E-mail"
+      rules={[
+        {
+          type: "email",
+          message: "В почте содержатся ошибки, попробуйте снова!",
+        },
+        {
+          required: true,
+          message: "Введите вашу почту!",
+        },
+      ]}
+    >
+      <Input className="form__input" />
     </Form.Item>
 
     <Form.Item
-      className="form__imput"
+      className="form__inner"
       label="Password"
       name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}>
-      <Input.Password />
+      rules={[{ required: true, message: "Введите ваш пароль!" }]}
+    >
+      <Input.Password className="form__input" />
     </Form.Item>
 
     <Button
-      btnClass={'button_type_login'}
+      btnClass={"button_type_login"}
       btnText="Войти"
       btnType="submit"
       // onClick={}
--- easy-meal/src/components/Register/Register.jsx
+++ easy-meal/src/components/Register/Register.jsx
@@ -1,13 +1,13 @@
-import {  Form, Input } from 'antd';
-import './Register.css';
-import Button from '../Button/Button';
+import { Form, Input } from "antd";
+import "./Register.css";
+import Button from "../Button/Button";
 
 const onFinish = (values) => {
-  console.log('Success:', values);
+  console.log("Success:", values);
 };
 
 const onFinishFailed = (errorInfo) => {
-  console.log('Failed:', errorInfo);
+  console.log("Failed:", errorInfo);
 };
 
 const Register = () => (
@@ -20,47 +20,53 @@ const Register = () => (
     // initialValues={{ remember: true }}
     onFinish={onFinish}
     onFinishFailed={onFinishFailed}
-    autoComplete="off">
-    <h3 className="form__title">Register</h3>
+    autoComplete="off"
+  >
+    <h3 className="form__title">Регистрация</h3>
 
     <Form.Item
+      className="form__inner"
       label="Username"
       name="username"
-      rules={[{ required: true, message: 'Please input your username!' }]}>
-      <Input className="form__imput" />
+      rules={[{ required: true, message: "Введите ваше имя!" }]}
+    >
+      <Input className="form__input" />
     </Form.Item>
 
     <Form.Item
+      className="form__inner"
       name="email"
       label="E-mail"
       rules={[
         {
-          type: 'email',
-          message: 'The input is not valid E-mail!',
+          type: "email",
+          message: "В почте содержатся ошибки, попробуйте снова!",
         },
         {
           required: true,
-          message: 'Please input your E-mail!',
+          message: "Введите вашу почту!",
         },
-      ]}>
-      <Input />
+      ]}
+    >
+      <Input className="form__input" />
     </Form.Item>
 
     <Form.Item
-      className="form__imput"
+      className="form__inner"
       label="Password"
       name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}>
-      <Input.Password />
+      rules={[{ required: true, message: "Введите ваш пароль!" }]}
+    >
+      <Input.Password className="form__input" />
     </Form.Item>
 
-       <Button
-          btnClass={'button_type_login'}
-          btnText="Зарегистрироваться"
-          btnType="submit"
-          // onClick={}
-        />
-      <Button/>
+    <Button
+      btnClass={"button_type_login"}
+      btnText="Зарегистрироваться"
+      btnType="submit"
+      // onClick={}
+    />
+    <Button />
   </Form>
 );
 
