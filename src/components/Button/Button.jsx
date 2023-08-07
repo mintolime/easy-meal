3616ab46df7cf8bb4f4cefad230d5741db75f074
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-08-07 13:32:10 +0700

    файлы были перемещены в основной каталог

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import '../Button/Button.css';
+
+function Button({ btnClass, btnType, btnText, onClick }) {
+  return (
+    <button className={`button ${btnClass}`} type={btnType} onClick={onClick}>
+      {btnText}
+    </button>
+  );
+}
+
+export default Button;
