0dac5c05295e17b701907acd4398f732cb24c738
Evgenia Smirnova <evgeniabright@yandex.ru>
2023-08-11 22:46:07 +0700

    fix:fixed the mealCategory

--- src/components/NewRecipe/NewRecipe.jsx
+++ src/components/NewRecipe/NewRecipe.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Form, Input, InputNumber, Space } from 'antd';
+import { Button, Form, Input, Select, Space } from 'antd';
 import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
 import { layout, validateMessages } from '../../utils/configAntDesign';
 
@@ -12,131 +12,132 @@ const NewRecipe = ({ onCreateRecipe }) => {
   return (
     // <div className="form">
 
-      <Form
+    <Form
       className="form"
-        {...layout}
-        name="nest-messages"
-        onFinish={onFinish}
-        style={{
-          maxWidth: 600,
-        }}
-        validateMessages={validateMessages}>
-         <h2 className="form__title">Новый рецепт</h2>
-         
-        <Form.Item name="mealAuthor" label="Автор">
-          <Input />
-        </Form.Item>
+      {...layout}
+      name="nest-messages"
+      onFinish={onFinish}
+      style={{
+        maxWidth: 600,
+      }}
+      validateMessages={validateMessages}>
+      <h2 className="form__title">Новый рецепт</h2>
 
-        <Form.Item name="mealName" label="Блюдо" rules={[{ required: true }]}>
-          <Input />
-        </Form.Item>
+      <Form.Item name="mealAuthor" label="Автор">
+        <Input />
+      </Form.Item>
 
-        <Form.Item name="mealCategory" label="Категория">
-          <Input />
-        </Form.Item>
+      <Form.Item name="mealName" label="Блюдо" rules={[{ required: true }]}>
+        <Input />
+      </Form.Item>
 
-        <Form.Item
-          name="mealSourceUrl"
-          label="Ссылка на источник"
-          rules={[{ type: 'url' }, { type: 'string', min: 6 }]}>
-          <Input />
-        </Form.Item>
+      <Form.Item name="mealCategory" label="Категория">
+        {/* <Input /> */}
+        <Select>
+          <Select.Option value="Салат">Салат</Select.Option>
+          <Select.Option value="Выпечка">Выпечка</Select.Option>
+          <Select.Option value="Паста">Паста</Select.Option>
+          <Select.Option value="Мясо">Мясо</Select.Option>
+        </Select>
+      </Form.Item>
 
-        <Form.Item
-          name="imageUrl"
-          label="Ссылка на изображение"
-          rules={[
-            {
-              required: true,
-            },
-            { type: 'url' },
-            { type: 'string', min: 6 },
-          ]}>
-          <Input />
-        </Form.Item>
+      <Form.Item
+        name="mealSourceUrl"
+        label="Ссылка на источник"
+        rules={[{ type: 'url' }, { type: 'string', min: 6 }]}>
+        <Input />
+      </Form.Item>
 
-        <Form.Item
-          name="youtubeUrl"
-          label="Ссылка на видео"
-          rules={[{ type: 'url' }, { type: 'string', min: 6 }]}>
-          <Input />
-        </Form.Item>
+      <Form.Item
+        name="imageUrl"
+        label="Ссылка на изображение"
+        rules={[
+          {
+            required: true,
+          },
+          { type: 'url' },
+          { type: 'string', min: 6 },
+        ]}>
+        <Input />
+      </Form.Item>
 
-        <Form.Item
-          name="instructions"
-          label="Способ приготовления"
-          rules={[
-            {
-              required: true,
-              message: 'Это поле пропущено',
-            },
-          ]}>
-          <Input.TextArea />
-        </Form.Item>
+      <Form.Item
+        name="youtubeUrl"
+        label="Ссылка на видео"
+        rules={[{ type: 'url' }, { type: 'string', min: 6 }]}>
+        <Input />
+      </Form.Item>
 
-        <Form.Item name="ingredients" label="Ингредиенты" rules={[{ required: true }]}>
+      <Form.Item
+        name="instructions"
+        label="Способ приготовления"
+        rules={[
+          {
+            required: true,
+            message: 'Это поле пропущено',
+          },
+        ]}>
+        <Input.TextArea />
+      </Form.Item>
 
-          <Form.List name="ingredients">
-            {(fields, { add, remove }) => (
-              <>
-                {fields.map(({ key, name, ...restField }) => (
-                  <Space
-                    key={key}
-                    style={{
-                      display: 'flex',
-                      marginBottom: 8,
-                    }}
-                    align="baseline">
-                    <Form.Item
-                      {...restField}
-                      name={[name, 'ingredient']}
-                      // name='Продукт'
-                      rules={[
-                        {
-                          required: true,
-                          message: 'Это поле пропущено',
-                        },
-                      ]}>
-                      <Input placeholder="Продукт" />
-                    </Form.Item>
-                    <Form.Item
-                      {...restField}
-                      name={[name, 'measure']}
-                      rules={[
-                        {
-                          required: true,
-                          message: 'Это поле пропущено',
-                        },
-                      ]}>
-                      <Input placeholder="Граммовка" />
-                    </Form.Item>
-                    <MinusCircleOutlined onClick={() => remove(name)} />
-                  </Space>
-                ))}
-                <Form.Item>
-                  <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
-                    Добавить ингредиент
-                  </Button>
-                </Form.Item>
-              </>
-            )}
-          </Form.List>
+      <Form.Item name="ingredients" label="Ингредиенты" rules={[{ required: true }]}>
+        <Form.List name="ingredients">
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map(({ key, name, ...restField }) => (
+                <Space
+                  key={key}
+                  style={{
+                    display: 'flex',
+                    marginBottom: 8,
+                  }}
+                  align="baseline">
+                  <Form.Item
+                    {...restField}
+                    name={[name, 'ingredient']}
+                    // name='Продукт'
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Это поле пропущено',
+                      },
+                    ]}>
+                    <Input placeholder="Продукт" />
+                  </Form.Item>
+                  <Form.Item
+                    {...restField}
+                    name={[name, 'measure']}
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Это поле пропущено',
+                      },
+                    ]}>
+                    <Input placeholder="Граммовка" />
+                  </Form.Item>
+                  <MinusCircleOutlined onClick={() => remove(name)} />
+                </Space>
+              ))}
+              <Form.Item>
+                <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                  Добавить ингредиент
+                </Button>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
+      </Form.Item>
 
-        </Form.Item>
-
-        <Form.Item
-          wrapperCol={{
-            ...layout.wrapperCol,
-            offset: 8,
-          }}>
-
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-
-        </Form.Item>
-
-      </Form>
+      <Form.Item
+        wrapperCol={{
+          ...layout.wrapperCol,
+          offset: 8,
+        }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
     // </div>
   );
 };
