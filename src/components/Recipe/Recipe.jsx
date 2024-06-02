23da9c0a993b5b0cc469cab7229e615c8f189d93
mintolime <evgeniabright@yandex.ru>
2024-06-02 21:51:49 +0700

    dev: added ‘Not found’ element for recipes

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Modal } from 'antd';
 import DOMPurify from 'dompurify';
 
@@ -9,6 +9,7 @@ import heartLiked from '../../images/icon__heart_liked.svg';
 import question from '../../images/question-help.svg';
 import dice from '../../images/dice_icon.svg';
 import AddToCart from '../../images/cart.svg';
+import { Link } from 'react-router-dom';
 
 const Recipe = ({ recipe, likedRecipes, getRandomRecipe, onLikeRecipe }) => {
   const [rotateDice, setRotateDice] = useState(false);
@@ -25,20 +26,21 @@ const Recipe = ({ recipe, likedRecipes, getRandomRecipe, onLikeRecipe }) => {
         <div style={{ fontFamily: 'Cruinn' }}>
           <p>
             Данный ресурс служит для быстрого решения вашей ежедневной головоломки: Что же
-            приготовить на ужин?😱
+            приготовить на ужин? 😱
           </p>
           <p>
             В нашем приложении вы можете увидеть сокращенную версию рецептов. Для перехода к
             источнику нажмите на "Полный рецепт"
           </p>
           <p>Для генерации рецепта нажмите кубик 🎲</p>
-          <p>Чтобы добавить понравившийся рецепт, нажмите на сердечко 🧡 </p>
+          <p>Чтобы добавить понравившийся рецепт, нажмите на сердечко 🧡</p>
           <p>Приятного пользования!</p>
         </div>
       ),
       onOk() {},
     });
   };
+
   // Почему-то при переходе с главной страницы на рецепты попадаешь в конец,
   // поэтому добавил принудильный скролл наверх
   useEffect(() => {
@@ -57,101 +59,103 @@ const Recipe = ({ recipe, likedRecipes, getRandomRecipe, onLikeRecipe }) => {
       setRotateDice(false);
     }, 600);
   };
-
+  console.log(recipe.length > 1);
   return (
     <section className="recipe">
-      <div className="recipe__buttons-container">
-        <Button
-          btnClass={`recipe__heart-btn ${isLiked && scaleHeart && 'scale'}`}
-          btnText={
-            <img
-              className="recipe__icon-heart"
-              src={isLiked ? heartLiked : heart}
-              alt="heart icon"
+      {recipe.length > 1 ? (
+        <>
+          <div className="recipe__buttons-container">
+            <Button
+              btnClass={`recipe__heart-btn ${isLiked && scaleHeart ? 'scale' : ''}`}
+              btnText={
+                <img
+                  className="recipe__icon-heart"
+                  src={isLiked ? heartLiked : heart}
+                  alt="heart icon"
+                  onClick={() => {
+                    animateButton('heart');
+                    onLikeRecipe(recipe, isLiked);
+                  }}
+                />
+              }
+            />
+
+            <Button
+              btnClass="recipe__question"
+              btnText={<img className="recipe__icon-question" src={question} alt="question icon" />}
+              onClick={showInfo}
+            />
+
+            <Button
+              btnClass={`recipe__dice-btn ${rotateDice ? 'rotate' : ''}`}
+              btnText={<img className="recipe__icon-dice" src={dice} alt="dice icon" />}
               onClick={() => {
-                animateButton('heart');
-                onLikeRecipe(recipe, isLiked);
+                animateButton('dice');
+                getRandomRecipe();
               }}
             />
-          }
-        />
+          </div>
+          <img
+            className="recipe__image"
+            loading="lazy"
+            src={recipe.imageUrl}
+            alt={recipe.mealName}
+          />
+          <div className="recipe__info recipe__box-shabow">
+            <h1 className="recipe__meal-name">{recipe.mealName}</h1>
 
-        <Button
-          btnClass="recipe__question"
-          btnText={<img className="recipe__icon-question" src={question} alt="question icon" />}
-          onClick={showInfo}
-        />
+            <div className="recipe__buttons-container recipe__buttons-container_flex-column">
+              {showInstructions ? (
+                <Button
+                  btnClass={'recipe__button'}
+                  btnText={'Ингредиенты'}
+                  onClick={() => setShowInstructions((prev) => !prev)}
+                />
+              ) : (
+                <Button
+                  btnClass={'recipe__button'}
+                  btnText={'Как готовить'}
+                  onClick={() => setShowInstructions((prev) => !prev)}
+                />
+              )}
 
-        <Button
-          btnClass={`recipe__dice-btn ${rotateDice && 'rotate'}`}
-          btnText={<img className="recipe__icon-dice" src={dice} alt="dice icon" />}
-          onClick={() => {
-            animateButton('dice');
-            getRandomRecipe();
-          }}
-        />
-      </div>
+              {recipe.youtubeUrl && (
+                <a
+                  className="recipe__button recipe__button-yt"
+                  href={recipe.youtubeUrl}
+                  target="_blank"
+                  rel="noreferrer">
+                  Видео
+                </a>
+              )}
+              <div className="recipe__buttons-container recipe__buttons-container_flex-column">
+                <p className="recipe__author">{recipe.mealAuthor || 'No Author'}</p>
+                <a
+                  className="recipe__author-link"
+                  href={recipe.mealSourceUrl}
+                  target="_blank"
+                  rel="noreferrer">
+                  Полный рецепт &#10132;
+                </a>
+              </div>
+            </div>
+          </div>
 
-      <img className="recipe__image" loading="lazy" src={recipe.imageUrl} alt={recipe.mealName} />
-      <div className="recipe__info recipe__box-shabow">
-        <h1 className="recipe__meal-name">{recipe.mealName}</h1>
-        {/* <p className="recipe__ingredients-quantity">
-          {recipe.ingredients?.length} ингредиентов
-        </p> */}
-
-        <div className="recipe__buttons-container recipe__buttons-container_flex-column">
           {showInstructions ? (
-            <Button
-              btnClass={'recipe__button'}
-              btnText={'Ингредиенты'}
-              onClick={() => setShowInstructions((prev) => !prev)}
+            <p
+              className="recipe__instructions recipe__box-shabow"
+              dangerouslySetInnerHTML={{ __html: cleanInstructions }}
             />
           ) : (
-            <Button
-              btnClass={'recipe__button'}
-              btnText={'Как готовить'}
-              onClick={() => setShowInstructions((prev) => !prev)}
-            />
-          )}
+            <ul className="recipe__ingredients">
+              {recipe.ingredients?.map((item, index) => (
+                <li className="recipe__ingreditent-container recipe__box-shabow" key={index}>
+                  <div className="recipe__ingreditent">
+                    <p className="recipe__ingreditent-name">{item.ingredient}</p>
+                    <p className="recipe__ingreditent-measure">{item.measure}</p>
+                  </div>
 
-          {recipe.youtubeUrl && (
-            <a
-              className="recipe__button recipe__button-yt"
-              href={recipe.youtubeUrl}
-              target="_blank"
-              rel="noreferrer">
-              Видео
-            </a>
-          )}
-          <div className="recipe__buttons-container recipe__buttons-container_flex-column ">
-            <p className="recipe__author">{recipe.mealAuthor || 'No Author'}</p>
-            <a
-              className="recipe__author-link"
-              href={recipe.mealSourceUrl}
-              target="_blank"
-              rel="noreferrer">
-              Полный рецепт &#10132;
-            </a>
-          </div>
-        </div>
-      </div>
-
-      {showInstructions ? (
-        <p
-          className="recipe__instructions recipe__box-shabow"
-          dangerouslySetInnerHTML={{ __html: cleanInstructions }}
-        />
-      ) : (
-        <ul className="recipe__ingredients ">
-          {recipe.ingredients?.map((item, index) => {
-            return (
-              <li className="recipe__ingreditent-container recipe__box-shabow" key={index}>
-                <div className="recipe__ingreditent">
-                  <p className="recipe__ingreditent-name">{item.ingredient}</p>
-                  <p className="recipe__ingreditent-measure">{item.measure}</p>
-                </div>
-
-                {/* <Button
+                  {/* <Button
                   btnClass="button_type-addtocart"
                   btnText={
                     <img
@@ -161,10 +165,21 @@ const Recipe = ({ recipe, likedRecipes, getRandomRecipe, onLikeRecipe }) => {
                     />
                   }
                 /> */}
-              </li>
-            );
-          })}
-        </ul>
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
+      ) : (
+        <div>
+          <p className="recipe__not-found">
+            Рецепты не найдены... На сегодня готовка отменяется, вы можете заказать себе любимых
+            крылешек 🍗
+          </p>
+          <Link className=" link-null button_type_back" to="/">
+            На главную
+          </Link>
+        </div>
       )}
     </section>
   );
