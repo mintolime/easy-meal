a136130949cd76286780daf50bfaa2cd04531520
mintolime <evgeniabright@yandex.ru>
2025-03-31 20:32:02 +0700

    added recipe box

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -54,10 +54,10 @@ const Recipe = ({ recipe, likedRecipes, getRandomRecipe, onLikeRecipe }) => {
     if (btn === 'heart') {
       setScaleHeart(true);
     } else if (btn === 'dice') {
-      // при клике на кубик возвращаем состояние скрытого списка ингредиентов 
-      // и инструкции по приготовлению 
-      setShowMoreIngredients(false)
-      setShowInstructions(false)
+      // при клике на кубик возвращаем состояние скрытого списка ингредиентов
+      // и инструкции по приготовлению
+      setShowMoreIngredients(false);
+      setShowInstructions(false);
       setRotateDice(true);
     }
 
@@ -69,108 +69,109 @@ const Recipe = ({ recipe, likedRecipes, getRandomRecipe, onLikeRecipe }) => {
 
   return (
     <section className="recipe">
-      <div className="recipe__buttons-container">
-        <Button
-          btnClass={`recipe__heart-btn ${isLiked && scaleHeart && 'scale'}`}
-          btnText={
-            <img
-              className="recipe__icon-heart"
-              src={isLiked ? heartLiked : heart}
-              alt="heart icon"
-              onClick={() => {
-                animateButton('heart');
-                onLikeRecipe(recipe, isLiked);
-              }}
-            />
-          }
-        />
+      <div className="recipe__box">
+        {' '}
+        <div className="recipe__buttons-container">
+          <Button
+            btnClass={`recipe__heart-btn ${isLiked && scaleHeart && 'scale'}`}
+            btnText={
+              <img
+                className="recipe__icon-heart"
+                src={isLiked ? heartLiked : heart}
+                alt="heart icon"
+                onClick={() => {
+                  animateButton('heart');
+                  onLikeRecipe(recipe, isLiked);
+                }}
+              />
+            }
+          />
 
-        <Button
-          btnClass="recipe__question"
-          btnText={<img className="recipe__icon-question" src={question} alt="question icon" />}
-          onClick={showInfo}
-        />
+          <Button
+            btnClass="recipe__question"
+            btnText={<img className="recipe__icon-question" src={question} alt="question icon" />}
+            onClick={showInfo}
+          />
 
-        <Button
-          btnClass={`recipe__dice-btn ${rotateDice && 'rotate'}`}
-          btnText={<img className="recipe__icon-dice" src={dice} alt="dice icon" />}
-          onClick={() => {
-            animateButton('dice');
-            getRandomRecipe();
-          }}
-        />
-      </div>
-
-      <img className="recipe__image" loading="lazy" src={recipe.imageUrl} alt={recipe.mealName} />
-      <div className="recipe__info recipe__box-shabow">
-        <h1 className="recipe__meal-name">{recipe.mealName}</h1>
-        {/* <p className="recipe__ingredients-quantity">
+          <Button
+            btnClass={`recipe__dice-btn ${rotateDice && 'rotate'}`}
+            btnText={<img className="recipe__icon-dice" src={dice} alt="dice icon" />}
+            onClick={() => {
+              animateButton('dice');
+              getRandomRecipe();
+            }}
+          />
+        </div>
+        <img className="recipe__image" loading="lazy" src={recipe.imageUrl} alt={recipe.mealName} />
+        <div className="recipe__info recipe__box-shabow">
+          <h1 className="recipe__meal-name">{recipe.mealName}</h1>
+          {/* <p className="recipe__ingredients-quantity">
           {recipe.ingredients?.length} ингредиентов
         </p> */}
 
-        <div className="recipe__buttons-container recipe__buttons-container_flex-column">
-          {showInstructions ? (
-            <Button
-              btnClass={'recipe__button'}
-              btnText={'Ингредиенты'}
-              onClick={() => setShowInstructions((prev) => !prev)}
-            />
-          ) : (
-            <Button
-              btnClass={'recipe__button'}
-              btnText={'Как готовить'}
-              onClick={() => setShowInstructions((prev) => !prev)}
-            />
-          )}
+          <div className="recipe__buttons-container recipe__buttons-container_flex-column">
+            {showInstructions ? (
+              <Button
+                btnClass={'recipe__button'}
+                btnText={'Ингредиенты'}
+                onClick={() => setShowInstructions((prev) => !prev)}
+              />
+            ) : (
+              <Button
+                btnClass={'recipe__button'}
+                btnText={'Как готовить'}
+                onClick={() => setShowInstructions((prev) => !prev)}
+              />
+            )}
 
-          {recipe.youtubeUrl && (
-            <a
-              className="recipe__button recipe__button-yt"
-              href={recipe.youtubeUrl}
-              target="_blank"
-              rel="noreferrer">
-              Видео
-            </a>
-          )}
-          <div className="recipe__buttons-container recipe__buttons-container_flex-column ">
-            <p className="recipe__author">{recipe.mealAuthor || 'No Author'}</p>
-            <a
-              className="recipe__author-link"
-              href={recipe.mealSourceUrl}
-              target="_blank"
-              rel="noreferrer">
-              Полный рецепт &#10132;
-            </a>
+            {recipe.youtubeUrl && (
+              <a
+                className="recipe__button recipe__button-yt"
+                href={recipe.youtubeUrl}
+                target="_blank"
+                rel="noreferrer">
+                Видео
+              </a>
+            )}
+            <div className="recipe__buttons-container recipe__buttons-container_flex-column ">
+              <p className="recipe__author">{recipe.mealAuthor || 'No Author'}</p>
+              <a
+                className="recipe__author-link"
+                href={recipe.mealSourceUrl}
+                target="_blank"
+                rel="noreferrer">
+                Полный рецепт &#10132;
+              </a>
+            </div>
           </div>
         </div>
+        {showInstructions ? (
+          <p
+            className="recipe__instructions recipe__box-shabow"
+            dangerouslySetInnerHTML={{ __html: cleanInstructions }}
+          />
+        ) : (
+          <ul className="recipe__ingredients">
+            {recipe.ingredients
+              ?.slice(0, showMoreIngredients ? recipe.ingredients.length : 6)
+              .map((item, index) => (
+                <li className="recipe__ingreditent-container recipe__box-shabow" key={index}>
+                  <div className="recipe__ingreditent">
+                    <p className="recipe__ingreditent-name">{item.ingredient}</p>
+                    <p className="recipe__ingreditent-measure">{item.measure}</p>
+                  </div>
+                </li>
+              ))}
+            {recipe.ingredients?.length > 6 && (
+              <Button
+                btnClass="recipe__button"
+                onClick={toggleShowMore}
+                btnText={showMoreIngredients ? 'Скрыть ▲' : 'Еще ▼'}
+              />
+            )}
+          </ul>
+        )}
       </div>
-
-      {showInstructions ? (
-        <p
-          className="recipe__instructions recipe__box-shabow"
-          dangerouslySetInnerHTML={{ __html: cleanInstructions }}
-        />
-      ) : (
-        <ul className="recipe__ingredients">
-          {recipe.ingredients
-            ?.slice(0, showMoreIngredients ? recipe.ingredients.length : 6)
-            .map((item, index) => (
-              <li className="recipe__ingreditent-container recipe__box-shabow" key={index}>
-                <div className="recipe__ingreditent">
-                  <p className="recipe__ingreditent-name">{item.ingredient}</p>
-                  <p className="recipe__ingreditent-measure">{item.measure}</p>
-                </div>
-              </li>
-            ))}
-          {recipe.ingredients?.length > 6 && (
-            <Button
-              btnClass="recipe__button"
-              onClick={toggleShowMore}
-              btnText={showMoreIngredients ? 'Скрыть ▲' : 'Еще ▼'}
-            />
-          )}
-        </ul>
-      )}
     </section>
   );
 };
