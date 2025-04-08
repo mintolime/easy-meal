f60954e2f9fc82af954b5d95d10fe2edf3a9b55f
mintolime <evgeniabright@yandex.ru>
2025-04-08 23:48:21 +0700

    added routes for all users

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,10 @@ const { celebrate } = require('celebrate');
 
 const { likeRecipeValidation } = require('../utils/validation');
 
-const { getMe, likeRecipe, dislikeRecipe } = require('../controllers/users');
+const { getMe, likeRecipe, dislikeRecipe, allUsers } = require('../controllers/users');
 
 usersRouter.get('/me', getMe);
+usersRouter.get('/usersAll', allUsers);
 usersRouter.post(
   '/like/:recipeId',
   celebrate(likeRecipeValidation),
