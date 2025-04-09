c6c366418463f787ca60ca2a3c669d7fe1116991
mintolime <evgeniabright@yandex.ru>
2025-04-09 15:58:22 +0700

    added error messages for authDeleteRecipe

--- middlewares/authDeleteRecipe.js
+++ middlewares/authDeleteRecipe.js
@@ -2,19 +2,19 @@
 const Recipe = require('../models/recipe');
 const customError = require('../errors');
 
+const ERROR = require('../utils/errorMessages');
+
 module.exports = (req, res, next) => {
   Recipe.findById({ _id: req.params._id })
     .then((recipe) => {
       // console.log(recipe.owner);
       if (!recipe) {
-        next(new customError.NotFound('Рецепта с указанным id не существует'));
+        next(new customError.NotFound(ERROR.RECIPE.NOT_FOUND));
       } else if (
         recipe.owner[recipe.owner.indexOf(req.user._id)].toHexString() !==
         req.user._id
       ) {
-        next(
-          new customError.Forbidden('У вас нет прав на удаление чужого рецепта')
-        );
+        next(new customError.Forbidden(ERROR.RECIPE.NO_RULES));
       }
       return next();
     })
