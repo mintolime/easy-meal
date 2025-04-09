6743f6b9fe202d267a7648c60b744c6246b126c7
mintolime <evgeniabright@yandex.ru>
2025-04-09 15:56:12 +0700

    added error messages for auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 const { Unauthorized } = require('../errors');
+const ERROR = require('../utils/errorMessages');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new Unauthorized('Необходима авторизация'));
+    return next(new Unauthorized(ERROR.LOGIN.UNAUTHORIZED));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -15,7 +16,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, config.jwtSecret);
   } catch (err) {
-    next(new Unauthorized('Необходима авторизация'));
+    next(new Unauthorized(ERROR.LOGIN.UNAUTHORIZED));
   }
 
   req.user = payload;
