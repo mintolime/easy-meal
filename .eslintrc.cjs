67210833025cba6e213ff1cc86c0e8cbf4da9d9b
mintolime <evgeniabright@yandex.ru>
2025-04-21 13:50:57 +0700

    fixed config .eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,6 @@ module.exports = {
     plugins: ['react-refresh'],
     rules: {
         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'react/prop-types': 'off',
     },
-    'react/prop-types': 'off',
 };
