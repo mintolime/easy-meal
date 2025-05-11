edec7b2abd1d882337cd04fb38d8cb317e25c144
mintolime <evgeniabright@yandex.ru>
2025-05-11 22:02:12 +0700

    added test users

--- src/components/admin/Users/Users.tsx
+++ src/components/admin/Users/Users.tsx
@@ -1,51 +1,38 @@
-import { Descriptions, DescriptionsProps } from 'antd';
+import { Descriptions } from 'antd';
 import classNames from 'classnames';
-import './Users.scss';
 import React from 'react';
+import './Users.scss';
+import { formatDate } from '../../../utils/functions';
 
 type Props = React.HTMLAttributes<HTMLElement> & {};
 const Users = ({ ...props }: Props) => {
-    const items: DescriptionsProps['items'] = [
-        {
-            key: '1',
-            label: 'UserName',
-            children: <p>Zhou Maomao</p>,
-        },
-        {
-            key: '2',
-            label: 'Telephone',
-            children: <p>1810000000</p>,
-        },
-        {
-            key: '3',
-            label: 'Live',
-            children: <p>Hangzhou, Zhejiang</p>,
-        },
-        {
-            key: '4',
-            label: 'Remark',
-            children: <p>empty</p>,
-        },
-        {
-            key: '5',
-            label: 'Address',
-            children: <p>No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China</p>,
-        },
-    ];
-
     return (
         <div className={classNames('users', props.className)}>
-            <Descriptions title="User Info" items={items} />;
-            <Descriptions title="User Info">
-                <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
-                <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
-                <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
-                <Descriptions.Item label="Remark">empty</Descriptions.Item>
-                <Descriptions.Item label="Address">
-                    No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-                </Descriptions.Item>
+            <Descriptions className="users__description" title="test@example.com">
+                <Descriptions.Item label="email">test@example.com</Descriptions.Item>
+                <Descriptions.Item label="createData">{formatDate('2025-04-08T17:11:34.302Z')}</Descriptions.Item>
             </Descriptions>
-            ;
+
+            <Descriptions className="users__description" title="test@example.com">
+                <Descriptions.Item label="email">test@example.com</Descriptions.Item>
+                <Descriptions.Item label="createData">{formatDate('2025-04-08T17:11:34.302Z')}</Descriptions.Item>
+            </Descriptions>
+            <Descriptions className="users__description" title="test@example.com">
+                <Descriptions.Item label="email">test@example.com</Descriptions.Item>
+                <Descriptions.Item label="createData">{formatDate('2025-04-08T17:11:34.302Z')}</Descriptions.Item>
+            </Descriptions>
+            <Descriptions className="users__description" title="test@example.com">
+                <Descriptions.Item label="email">test@example.com</Descriptions.Item>
+                <Descriptions.Item label="createData">{formatDate('2025-04-08T17:11:34.302Z')}</Descriptions.Item>
+            </Descriptions>
+            <Descriptions className="users__description" title="test@example.com">
+                <Descriptions.Item label="email">test@example.com</Descriptions.Item>
+                <Descriptions.Item label="createData">{formatDate('2025-04-08T17:11:34.302Z')}</Descriptions.Item>
+            </Descriptions>
+            <Descriptions className="users__description" title="test@example.com">
+                <Descriptions.Item label="email">test@example.com</Descriptions.Item>
+                <Descriptions.Item label="createData">{formatDate('2025-04-08T17:11:34.302Z')}</Descriptions.Item>
+            </Descriptions>
         </div>
     );
 };
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -62,3 +62,20 @@ export const modifyRecipeObject = (value) => {
 
     return newRecipe;
 };
+
+export const formatDate = (isoDateString) => {
+  const date = new Date(isoDateString);
+  
+  // Проверка на валидность даты
+  if (isNaN(date.getTime())) {
+    return "Invalid Date";
+  }
+  
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  
+  return `${day}.${month}.${year} ${hours}:${minutes}`;
+}
