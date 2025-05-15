1aa5a6a5b989f5f063dc7478dcfc42a4c0b58ed8
mintolime <evgeniabright@yandex.ru>
2025-05-15 19:12:15 +0700

    added animation for question btn

--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -29,17 +29,20 @@ const Recipe = ({ recipe, likedRecipes, getRandomRecipe, onLikeRecipe }) => {
         setShowMoreIngredients(!showMoreIngredients);
     };
 
-    // const itemVariants = {
-    //   hidden: { y: 20, opacity: 0 },
-    //   visible: {
-    //     y: 0,
-    //     opacity: 1,
-    //     transition: {
-    //       duration: 0.5,
-    //       ease: 'easeOut',
-    //     },
-    //   },
-    // };
+    const pulse = {
+      rest: { scale: 1 },
+      hover: { scale: 1.1, transition: { duration: 0.3 } },
+      tap: { scale: 0.9, transition: { duration: 0.1 } },
+      animate: {
+          scale: [1.25, 1.1, 1.25, 1.1], 
+          transition: {
+              duration: 1.25, 
+              ease: 'easeInOut',
+              repeat: Infinity,
+              repeatType: 'reverse',
+          },
+      },
+  };
 
     const imageVariants = {
         hidden: { scale: 0.9, opacity: 0 },
@@ -128,11 +131,25 @@ const Recipe = ({ recipe, likedRecipes, getRandomRecipe, onLikeRecipe }) => {
                             />
                         </motion.div>
 
-                        <Button
-                            btnClass="recipe__question"
-                            btnText={<img className="recipe__icon-question" src={question} alt="question icon" />}
+                        <motion.button
+                            className="recipe__question"
                             onClick={showInfo}
-                        />
+                            initial="rest"
+                            whileHover="hover"
+                            whileTap="tap"
+                            animate="animate"
+                        >
+                            <motion.svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                height="48"
+                                viewBox="0 -960 960 960"
+                                width="48"
+                                className="recipe__icon-question"
+                                variants={pulse}
+                            >
+                                <path d="M484-247q16 0 27-11t11-27q0-16-11-27t-27-11q-16 0-27 11t-11 27q0 16 11 27t27 11Zm-35-146h59q0-26 6.5-47.5T555-490q31-26 44-51t13-55q0-53-34.5-85T486-713q-49 0-86.5 24.5T345-621l53 20q11-28 33-43.5t52-15.5q34 0 55 18.5t21 47.5q0 22-13 41.5T508-512q-30 26-44.5 51.5T449-393Zm31 313q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z" />
+                            </motion.svg>
+                        </motion.button>
 
                         <motion.div
                             animate={{ rotate: isDiceRotating ? 360 : 0 }}
@@ -195,33 +212,6 @@ const Recipe = ({ recipe, likedRecipes, getRandomRecipe, onLikeRecipe }) => {
                             </div>
                         </div>
                     </div>
-                    {/* {showInstructions ? (
-          <p
-            className="recipe__instructions recipe__box-shabow"
-            dangerouslySetInnerHTML={{ __html: cleanInstructions }}
-          />
-        ) : (
-          <ul className="recipe__ingredients">
-            {recipe.ingredients
-              ?.slice(0, showMoreIngredients ? recipe.ingredients.length : 6)
-              .map((item, index) => (
-                <li className="recipe__ingreditent-container recipe__box-shabow" key={index}>
-                  <div className="recipe__ingreditent">
-                    <p className="recipe__ingreditent-name">{item.ingredient}</p>
-                    <p className="recipe__ingreditent-measure">{item.measure}</p>
-                  </div>
-                </li>
-              ))}
-            {recipe.ingredients?.length > 6 && (
-              <Button
-                btnClass="recipe__button"
-                onClick={toggleShowMore}
-                btnText={showMoreIngredients ? 'Скрыть ▲' : 'Еще ▼'}
-              />
-            )}
-          </ul>
-        )}
-      </div> */}
 
                     <AnimatePresence mode="wait">
                         {showInstructions ? (
